751395047aa295c63e56cbe5367cdd3cf7c54d99
André <andrejusmattos@gmail.com>
2024-06-07 15:23:58 -0300

    Ex 7: DELETE CONSULTA, PACIENTE E NUTRICIONISTA

---
+++ [M205] Ex 7 - DELETE CONSULTA.sql
@@ -0,0 +1,22 @@
+INSERT INTO consulta(
+	paciente_id, nutri_id, observacoes) VALUES
+	(2, 1, 'Consulta inicial para avaliação nutricional'),
+	(3, 2, 'Acompanhamento nutricional para atleta profissional'),
+	(4, 3, 'Consulta de rotina para controle de peso e saúde');
+
+SELECT * FROM consulta;
+
+UPDATE consulta
+SET data = '2024-06-07'
+WHERE consulta_id = 4;
+
+UPDATE consulta
+SET data = '2024-10-07'
+WHERE consulta_id = 6;
+
+UPDATE consulta
+SET observacoes = 'Consulta de rotina para controle de peso'
+WHERE consulta_id = 6;
+
+DELETE FROM consulta
+WHERE nutri_id = 2;
---
+++ [M205] Ex 7 - DELETE NUTRICIONISTA.sql
@@ -0,0 +1,22 @@
+INSERT INTO nutricionista(
+	crn, especialidade, tempo_experiencia) VALUES
+	('123456/SC', 'nutrição funcional', 3),
+	('654321/SP', 'nutrição esportiva', 5),
+	('789012/RJ', 'nutrição clínica', 8);
+
+SELECT * FROM nutricionista;
+
+UPDATE nutricionista
+SET crn = '789012/MG'
+WHERE tempo_experiencia = 8;
+
+UPDATE nutricionista
+SET especialidade = 'nutrição vegana'
+WHERE nutri_id = 3;
+
+UPDATE nutricionista
+SET tempo_experiencia = 4
+WHERE nutri_id = 1;
+
+DELETE FROM nutricionista
+WHERE crn LIKE '%SP';
